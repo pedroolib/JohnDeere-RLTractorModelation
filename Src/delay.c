cafa46a34b47a0cc9a2aa0b8610cd2bcc7b79ce5
pedroolib <pedro.uribe@cetys.edu.mx>
2026-05-14 13:47:52 -0600

    LCD JALANDO

--- Src/delay.c
+++ Src/delay.c
@@ -10,48 +10,48 @@
 
 void USER_TIM2_Delay_40ms(void){
 	__asm(" 		ldr r0, =512000UL	");//	~40 ms @ 64 MHz
-	__asm(" again:	sub r0, r0, #1		");
-	__asm("			cmp r0, #0			");
-	__asm("			bne again			");
-	__asm("			nop					");
+	__asm(" again_40ms:\tsub r0, r0, #1\t\t");
+	__asm("         \tcmp r0, #0          ");
+	__asm("         \tbne again_40ms      ");
+	__asm("         \tnop                 ");
 }
 
 void USER_TIM_Delay_4_1ms(void){
 	__asm(" 		ldr r0, =52480UL	");//	~4.1 ms @ 64 MHz
-	__asm(" again:	sub r0, r0, #1		");
-	__asm("			cmp r0, #0			");
-	__asm("			bne again			");
-	__asm("			nop					");
+	__asm(" again_4_1ms:\tsub r0, r0, #1\t\t");
+	__asm("         \tcmp r0, #0          ");
+	__asm("         \tbne again_4_1ms     ");
+	__asm("         \tnop                 ");
 }
 
 void USER_TIM_Delay_1ms(void){
 	__asm(" 		ldr r0, =12800UL	");//	~1 ms @ 64 MHz
-	__asm(" again:	sub r0, r0, #1		");
-	__asm("			cmp r0, #0			");
-	__asm("			bne again			");
-	__asm("			nop					");
+	__asm(" again_1ms:\tsub r0, r0, #1\t\t");
+	__asm("         \tcmp r0, #0          ");
+	__asm("         \tbne again_1ms       ");
+	__asm("         \tnop                 ");
 }
 
 void USER_TIM_Delay_100us(void){
-	__asm(" 		ldr r0, =1280UL		");//	~100 us @ 64 MHz
-	__asm(" again:	sub r0, r0, #1		");
-	__asm("			cmp r0, #0			");
-	__asm("			bne again			");
-	__asm("			nop					");
+	__asm(" 		ldr r0, =1280UL\t\t");//	~100 us @ 64 MHz
+	__asm(" again_100us:\tsub r0, r0, #1\t\t");
+	__asm("         \tcmp r0, #0          ");
+	__asm("         \tbne again_100us     ");
+	__asm("         \tnop                 ");
 }
 
 void USER_TIM_Delay_53us(void){
-	__asm(" 		ldr r0, =680UL		");//	~53 us @ 64 MHz
-	__asm(" again:	sub r0, r0, #1		");
-	__asm("			cmp r0, #0			");
-	__asm("			bne again			");
-	__asm("			nop					");
+	__asm(" 		ldr r0, =680UL\t\t");//	~53 us @ 64 MHz
+	__asm(" again_53us:\tsub r0, r0, #1\t\t");
+	__asm("         \tcmp r0, #0          ");
+	__asm("         \tbne again_53us      ");
+	__asm("         \tnop                 ");
 }
 
 void USER_TIM_Delay_10us(void){
-	__asm(" 		ldr r0, =128UL		");//	~10 us @ 64 MHz
-	__asm(" again:	sub r0, r0, #1		");
-	__asm("			cmp r0, #0			");
-	__asm("			bne again			");
-	__asm("			nop					");
+	__asm(" 		ldr r0, =128UL\t\t");//	~10 us @ 64 MHz
+	__asm(" again_10us:\tsub r0, r0, #1\t\t");
+	__asm("         \tcmp r0, #0          ");
+	__asm("         \tbne again_10us      ");
+	__asm("         \tnop                 ");
 }
